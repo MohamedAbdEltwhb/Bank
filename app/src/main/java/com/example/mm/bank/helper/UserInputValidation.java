e995be37e4c79a19ac3af63f2ec692fe06bbbccc
MohamedAbdEltwhb <gethubmmdabd6@gmail.com111>
2019-02-20 14:04:52 +0200

    add input Validation Class

--- app/src/main/java/com/example/mm/bank/helper/UserInputValidation.java
+++ app/src/main/java/com/example/mm/bank/helper/UserInputValidation.java
@@ -1,5 +1,54 @@
 package com.example.mm.bank.helper;
 
+import android.support.design.widget.TextInputLayout;
+import android.util.Patterns;
+
 public class UserInputValidation {
-    
+
+    /**
+     *
+     * @InputValidation Class Constants
+     *
+     *  (?=.*\d)      #   must contains one digit from 0-9
+     *  (?=.*[a-z])   #   must contains one lowercase characters
+     *  (?=.*[A-Z])   #   must contains one uppercase characters
+     *  (?=.*[@#$%])  #   must contains one special symbols in the list "@#$%"
+     *          .     #   match anything with previous condition checking
+     *  {6,20}        #   length at least 6 characters and maximum of 20
+     * */
+
+    public static boolean isValidMail(String emailInput) {
+        if (emailInput.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()){
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isValidPassword(String passwordInput) {
+        if (passwordInput.isEmpty() || passwordInput.length() < 3){
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isValidRePassword (String rePassword, String password){
+        if (rePassword.isEmpty() || !rePassword.contentEquals(password)){
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isValidMobile (String number){
+        if (number.isEmpty() || !Patterns.PHONE.matcher(number).matches() || number.length() < 10 || number.length() > 11){
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isValidName (String name){
+        if (name.isEmpty() || name.length() < 3 || name.length() > 20){
+            return false;
+        }
+        return true;
+    }
 }
--- app/src/main/java/com/example/mm/bank/ui/fragments/userCycle/RegisterFragment.java
+++ app/src/main/java/com/example/mm/bank/ui/fragments/userCycle/RegisterFragment.java
@@ -3,6 +3,8 @@ package com.example.mm.bank.ui.fragments.userCycle;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
@@ -15,6 +17,7 @@ import android.widget.Toast;
 
 import com.example.mm.bank.R;
 import com.example.mm.bank.helper.HelperMethod;
+import com.example.mm.bank.helper.UserInputValidation;
 import com.example.mm.bank.ui.activities.HomeCycleActivity;
 import com.example.mm.bank.ui.custom.CustomSpinnerItem;
 
@@ -30,11 +33,25 @@ public class RegisterFragment extends Fragment implements AdapterView.OnItemSele
 
     Unbinder unbinder;
 
-
     @BindView(R.id.Register_Fragment_Spinner_Cities)
     Spinner RegisterFragmentSpinnerCities;
     @BindView(R.id.Register_Fragment_Spinner_Blood_Type)
     Spinner RegisterFragmentSpinnerBloodType;
+
+    @BindView(R.id.Register_Fragment_TiL_Name)
+    TextInputLayout RegisterFragmentTiLName;
+    @BindView(R.id.Register_Fragment_TiL_Email)
+    TextInputLayout RegisterFragmentTiLEmail;
+    @BindView(R.id.Register_Fragment_TiL_BirthDate)
+    TextInputLayout RegisterFragmentTiLBirthDate;
+    @BindView(R.id.Register_Fragment_TiL_Last_Blood_Donation)
+    TextInputLayout RegisterFragmentTiLLastBloodDonation;
+    @BindView(R.id.Register_Fragment_TiL_Phone)
+    TextInputLayout RegisterFragmentTiLPhone;
+    @BindView(R.id.Register_Fragment_TiL_Password)
+    TextInputLayout RegisterFragmentTiLPassword;
+    @BindView(R.id.Register_Fragment_TiL_Re_Password)
+    TextInputLayout RegisterFragmentTiLRePassword;
 
 
     private Toolbar mToolbar;
@@ -60,9 +77,36 @@ public class RegisterFragment extends Fragment implements AdapterView.OnItemSele
         chickSpinner(RegisterFragmentSpinnerBloodType,
                 HelperMethod.setSpinnerBloodType(getContext(), RegisterFragmentSpinnerBloodType));
 
-        chickSpinner(RegisterFragmentSpinnerCities,
-                HelperMethod.setSpinnerCities(getContext(), RegisterFragmentSpinnerCities));
         return view;
+    }
+
+    private void extractInputValidation() {
+        String name = RegisterFragmentTiLName.getEditText().getText().toString();
+        String email = RegisterFragmentTiLEmail.getEditText().getText().toString().trim();
+        String birthDate = RegisterFragmentTiLBirthDate.getEditText().getText().toString().trim();
+        String lastBloodDonation = RegisterFragmentTiLLastBloodDonation.getEditText().getText().toString();
+        String phone = RegisterFragmentTiLPhone.getEditText().getText().toString().trim();
+        String password = RegisterFragmentTiLPassword.getEditText().getText().toString().trim();
+        String rePassword = RegisterFragmentTiLRePassword.getEditText().getText().toString().trim();
+
+        if (!UserInputValidation.isValidName(name)){
+            RegisterFragmentTiLName.setError("Please Enter Correct Name..");
+        } else if (!UserInputValidation.isValidMail(email)){
+            RegisterFragmentTiLEmail.setError("Please Enter Correct Email..");
+        } else if (birthDate.isEmpty()){
+            RegisterFragmentTiLBirthDate.setError("Please Enter Your Birth Date..");
+        } else if (lastBloodDonation.isEmpty()){
+            RegisterFragmentTiLLastBloodDonation.setError("Please Enter Last Blood Donation..");
+        } else if (!UserInputValidation.isValidMobile(phone)){
+            RegisterFragmentTiLPhone.setError("Please Enter Correct Phone Number..");
+        } else if (!UserInputValidation.isValidPassword(password)){
+            RegisterFragmentTiLPassword.setError("Please Enter Strong Password..");
+        } else if (!UserInputValidation.isValidRePassword(rePassword, password)){
+            RegisterFragmentTiLRePassword.setError("Re Password Is not equal Password..");
+        }else {
+
+        }
+
     }
 
     private void chickSpinner(Spinner spinner, Boolean b) {
@@ -91,7 +135,8 @@ public class RegisterFragment extends Fragment implements AdapterView.OnItemSele
 
     @OnClick(R.id.Register_fragment_Sign_up_btn)
     public void onViewClicked() {
-
         getActivity().startActivity(new Intent(getActivity(), HomeCycleActivity.class));
     }
+
+
 }
