c58e37190015df6a0e7146b001080e76ae6e1dda
MohamedAbdEltwhb <gethubmmdabd6@gmail.com111>
2019-02-21 14:56:40 +0200

    add input Validation Class

--- app/src/main/java/com/example/mm/bank/data/rest/ApiServices.java
+++ app/src/main/java/com/example/mm/bank/data/rest/ApiServices.java
@@ -2,6 +2,7 @@ package com.example.mm.bank.data.rest;
 
 import com.example.mm.bank.data.model.cities.Cities;
 import com.example.mm.bank.data.model.governorates.Governorates;
+import com.example.mm.bank.data.model.register.Register;
 
 import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
@@ -20,8 +21,19 @@ public interface ApiServices {
     Call<Governorates> getGovernments();
 
     @GET("cities")
-    Call<Cities> getCities(@Query("governorate_id") String government_id);
+    Call<Cities> getCities(@Query("governorate_id") int government_id);
 
+    @POST("register")
+    @FormUrlEncoded
+    Call<Register> addUserRegisteration (@Field("name") String Register,
+                                         @Field("email") String email,
+                                         @Field("birth_date") String birth_date,
+                                         @Field("city_id") String city_id,
+                                         @Field("phone") String phone,
+                                         @Field("donation_last_date") String donation_last_date,
+                                         @Field("password") String password,
+                                         @Field("password_confirmation") String password_confirmation,
+                                         @Field("blood_type") String blood_type);
 
 
 //    @GET("governorates")
